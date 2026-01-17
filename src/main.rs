411ae8f283b704126d6a0b443ccbe523922c4e12
Nolhan <nolhan.dev@gmail.com>
2026-01-17 17:58:35 +0100

    feat(config): added config parser

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
+use dotenvy::dotenv;
 use crate::{
-  utils::logger::Logger,
+  utils::{
+    logger::Logger,
+    config::Config
+  },
 };
+use crate::utils::crypto::generate_key;
 
 fn main() {
+  dotenv().ok();
+
   Logger::info("Hello World!");
   Logger::highlight("Hello World!");
   Logger::warn("Hello World!");
   Logger::error("Hello World!");
+
+  let config = Config::new();
+  
+  let key = generate_key();
+  
+  println!("{:?}", key);
 }
 
 mod utils;
---
+++ src/utils/config.rs
@@ -0,0 +1,196 @@
+use std::{
+  env,
+  fs::File,
+  path::Path,
+  io::Read,
+};
+use std::collections::HashMap;
+use fancy_regex::Regex;
+
+enum DatastoreType {
+  FileSystem
+}
+
+struct Datastore {
+  storage_type: DatastoreType,
+  path: String,
+}
+
+struct Backup {
+  display_name: String,
+  connection_string: String,
+  store: Datastore,
+  schedule: Option<String>,
+  encryption_key: Option<String>,
+}
+
+#[derive(Debug)]
+enum TomlValue {
+  String(String),
+  Int(i32),
+  Bool(bool),
+  Object(Box<TomlValue>),
+  None
+}
+
+#[derive(Debug)]
+struct TomlProperty {
+  name: String,
+  value: TomlValue,
+}
+
+pub struct Config {
+  backups: HashMap<String, Backup>
+}
+
+impl Config {
+  pub fn new () -> Self {
+    let mut instance = Self {
+      backups: HashMap::new()
+    };
+
+    let config_file = env::var("CONFIG_FILE")
+      .unwrap_or("./config.toml".to_string());
+
+    let path = Path::new(&config_file);
+    let mut file = match File::open(path) {
+      Err(err) => panic!("Couldn't open config file {}: {}", path.display(), err),
+      Ok(file) => file,
+    };
+
+    let mut content = String::new();
+    match file.read_to_string(&mut content) {
+      Err(err) => panic!("Couldn't read file {}: {}", path.display(), err),
+      Ok(_) => instance.parse_config(content),
+    };
+
+    instance
+  }
+
+  fn parse_config (&mut self, config: String) {
+    let lines = config.lines();
+    let table_regex = Regex::new(r"^\[([\w.]+)]$").unwrap();
+    let mut blocs: HashMap<String, HashMap<String, TomlValue>> = HashMap::new();
+    let mut current_bloc_key = "".to_string();
+    blocs.entry(current_bloc_key.clone()).insert_entry(HashMap::new());
+
+    for (index, line) in lines.enumerate() {
+      let line = line.trim();
+
+      if table_regex.is_match(line).unwrap() {
+        let captures = table_regex.captures(line).unwrap().unwrap();
+        let backup_name = captures.get(1).map_or("", |m| m.as_str().split(".").last().unwrap());
+
+        blocs.entry(backup_name.to_string()).insert_entry(HashMap::new());
+        current_bloc_key = backup_name.to_string();
+      } else {
+        let property = match Self::parse_property(line.trim(), index as u16) {
+          Ok(value) => value,
+          Err(error) => panic!("{}", error),
+        };
+        println!("{:?}", property);
+        let current_bloc_key = current_bloc_key.clone();
+
+        if !matches!(property.value, TomlValue::None) {
+          blocs
+            .get_mut(current_bloc_key.as_str())
+            .unwrap()
+            .entry(property.name)
+            .insert_entry(property.value);
+        }
+      }
+    }
+
+    for (backup_name, properties) in blocs.iter() {
+      if backup_name == "" {
+        continue
+      }
+
+      let backup_name = match properties.get("display_name").unwrap_or(&TomlValue::String(backup_name.to_string())) {
+        TomlValue::String(value) => value.to_string(),
+        _ => panic!("Expected String value for property `display_name`")
+      };
+      let connection_string = match properties.get("connection_string") {
+        Some(TomlValue::String(value)) => value.to_string(),
+        _ => panic!("Expected String value for property `connection_string`")
+      };
+      let schedule = match properties.get("schedule") {
+        Some(TomlValue::String(value)) => Option::from(value.to_string()),
+        Some(TomlValue::None) | None => None,
+        _ => panic!("Expected String or None value for property `schedule`")
+      };
+      let encryption_key = match properties.get("encryption") {
+        Some(TomlValue::String(value)) => Option::from(value.to_string()),
+        Some(TomlValue::None) | None => None,
+        _ => panic!("Expected String or None value for property `encryption_key`")
+      };
+
+      if encryption_key.as_deref().is_some_and(|k| k.len() != 64) {
+        panic!("`encryption_key` is invalid. You can use the `mdbmcli generate-key` command to get one.")
+      }
+
+      self.backups.entry(backup_name.clone()).insert_entry(Backup {
+        display_name: backup_name,
+        connection_string,
+        store: Datastore {
+          storage_type: DatastoreType::FileSystem,
+          path: "/var/lib/mbm".to_string()
+        },
+        schedule,
+        encryption_key,
+      });
+    }
+  }
+
+  fn parse_property (line: &str, index: u16) -> Result<TomlProperty, String> {
+    let property_regex = Regex::new(r"^\s*(([\w-]+)\s*=\s*(.*))\s*$").map_err(|e| e.to_string())?;
+
+    if line.starts_with("#") || line.is_empty() {
+      return Ok(TomlProperty {
+        name: "".to_string(),
+        value: TomlValue::None
+      })
+    }
+
+    let captures = property_regex.captures(line).unwrap_or(None);
+    if captures.is_none() {
+      return Err(format!("Invalid configuration at line {}", index + 1))
+    }
+    let captures = captures.unwrap();
+
+    let property_name = captures.get(2).map_or("", |m| m.as_str());
+    let property_raw_value = captures.get(3).map_or("", |m| m.as_str());
+
+    let string_value_regex = Regex::new(r#"^(['"])([^'"]*)\1$"#).map_err(|e| e.to_string())?;
+    let int_value_regex = Regex::new(r"^[0-9]+$").map_err(|e| e.to_string())?;
+    let bool_value_regex = Regex::new(r"^(true)|(false)$").map_err(|e| e.to_string())?;
+
+    if string_value_regex.is_match(property_raw_value).unwrap() {
+      let property_raw_value = string_value_regex.captures(property_raw_value).unwrap().unwrap();
+      let property_value = property_raw_value.get(2).map_or("", |m| m.as_str());
+
+      Ok(TomlProperty {
+        name: property_name.to_string(),
+        value: TomlValue::String(property_value.to_string())
+      })
+    } else if int_value_regex.is_match(property_raw_value).unwrap() {
+      let property_raw_value = int_value_regex.captures(property_raw_value).unwrap().unwrap();
+      let property_value = property_raw_value.get(0).map_or(0, |m| m.as_str().parse().unwrap());
+
+      Ok(TomlProperty {
+        name: property_name.to_string(),
+        value: TomlValue::Int(property_value)
+      })
+    } else if bool_value_regex.is_match(property_raw_value).unwrap() {
+      let property_raw_value = bool_value_regex.captures(property_raw_value).unwrap().unwrap();
+      let property_value = property_raw_value.get(0).map_or(false, |m| m.as_str() == "true");
+
+      Ok(TomlProperty {
+        name: property_name.to_string(),
+        value: TomlValue::Bool(property_value)
+      })
+    } else {
+      Err(format!("Invalid property type at line {}", index + 1))
+    }
+  }
+}
