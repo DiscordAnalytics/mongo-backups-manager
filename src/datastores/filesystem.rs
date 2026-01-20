22127bd7cc827d01f3f4e2a4b7ed30eb0297fa22
Nolhan <nolhan.dev@gmail.com>
2026-01-20 09:28:27 +0100

    feat(datastores): added new & get_object to fs datastore

---
+++ src/datastores/filesystem.rs
@@ -0,0 +1,44 @@
+use std::fs::{File, create_dir_all};
+use std::io::{Bytes, Read};
+use std::path::{Path, PathBuf};
+use crate::datastores::Datastore;
+
+pub struct FilesystemDatastore {
+  base_path: PathBuf,
+}
+
+impl Datastore for FilesystemDatastore {
+  fn new(base_path: &str) -> Self {
+    let mut instance = Self {
+      base_path: PathBuf::from(base_path)
+    };
+
+    if !instance.base_path.exists() {
+      let _ = create_dir_all(instance.base_path.clone()).map_err(|err| panic!("Cannot create datastore base directory: {}", err));
+    }
+
+    instance
+  }
+
+  fn get_object(&self, path: String) -> Result<String, String> {
+    let full_path = self.base_path.join(Path::new(path.as_str()));
+
+    let mut file = File::open(full_path.display().to_string())
+      .map_err(|err| format!("Couldn't open file {}: {}", full_path.display(), err))?;
+
+    let mut content = String::new();
+    file.read_to_string(&mut content)
+      .map_err(|err| format!("Couldn't read file {}: {}", full_path.display(), err))?;
+
+
+    Ok(content)
+  }
+
+  fn list_objects(path: &str) -> Result<Vec<String>, String> {
+    todo!()
+  }
+
+  fn put_object(path: &str, content: Vec<Bytes<u8>>) -> Result<(), String> {
+    todo!()
+  }
+}
