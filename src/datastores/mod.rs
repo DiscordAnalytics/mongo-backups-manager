2195f6eae137768ec390ca6a9df9e6cfdb976464
Nolhan <nolhan.dev@gmail.com>
2026-01-20 09:39:19 +0100

    lint(datastores): cargo fmt

--- src/datastores/mod.rs
+++ src/datastores/mod.rs
@@ -4,9 +4,9 @@ pub mod filesystem;
 pub use filesystem::FilesystemDatastore;
 
 pub trait Datastore {
-  fn new(base_path: &str) -> Self;
+    fn new(base_path: &str) -> Self;
 
-  fn get_object(&self, path: String) -> Result<String, String>;
-  fn list_objects(path: &str) -> Result<Vec<String>, String>;
-  fn put_object(path: &str, content: Vec<Bytes<u8>>) -> Result<(), String>;
+    fn get_object(&self, path: String) -> Result<String, String>;
+    fn list_objects(path: &str) -> Result<Vec<String>, String>;
+    fn put_object(path: &str, content: Vec<Bytes<u8>>) -> Result<(), String>;
 }
