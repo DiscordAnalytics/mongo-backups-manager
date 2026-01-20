d1e8bc9cfb932603f41f56d6b718520b4ed6cbc0
Nolhan <nolhan.dev@gmail.com>
2026-01-20 11:06:43 +0100

    feat(datastores): added list_objects to fs datastore

--- src/datastores/filesystem.rs
+++ src/datastores/filesystem.rs
@@ -1,44 +1,59 @@
 use crate::datastores::Datastore;
-use std::fs::{File, create_dir_all};
+use regex::Regex;
+use std::fs::{File, create_dir_all, read_dir};
 use std::io::{Bytes, Read};
 use std::path::{Path, PathBuf};
 
 pub struct FilesystemDatastore {
-    base_path: PathBuf,
+  base_path: PathBuf,
 }
 
 impl Datastore for FilesystemDatastore {
-    fn new(base_path: &str) -> Self {
-        let mut instance = Self {
-            base_path: PathBuf::from(base_path),
-        };
+  fn new(base_path: &str) -> Self {
+    let mut instance = Self {
+      base_path: PathBuf::from(base_path),
+    };
 
-        if !instance.base_path.exists() {
-            let _ = create_dir_all(instance.base_path.clone())
-                .map_err(|err| panic!("Cannot create datastore base directory: {}", err));
-        }
-
-        instance
+    if !instance.base_path.is_dir() {
+      panic!("Datastore is not a directory")
     }
 
-    fn get_object(&self, path: String) -> Result<String, String> {
-        let full_path = self.base_path.join(Path::new(path.as_str()));
-
-        let mut file = File::open(full_path.display().to_string())
-            .map_err(|err| format!("Couldn't open file {}: {}", full_path.display(), err))?;
-
-        let mut content = String::new();
-        file.read_to_string(&mut content)
-            .map_err(|err| format!("Couldn't read file {}: {}", full_path.display(), err))?;
-
-        Ok(content)
+    if !instance.base_path.exists() {
+      let _ = create_dir_all(instance.base_path.clone())
+        .map_err(|err| panic!("Cannot create datastore base directory: {}", err));
     }
 
-    fn list_objects(path: &str) -> Result<Vec<String>, String> {
-        todo!()
-    }
+    instance
+  }
 
-    fn put_object(path: &str, content: Vec<Bytes<u8>>) -> Result<(), String> {
-        todo!()
-    }
+  fn get_object(&self, path: String) -> Result<String, String> {
+    let full_path = self.base_path.join(Path::new(path.as_str()));
+
+    let mut file = File::open(full_path.display().to_string())
+      .map_err(|err| format!("Couldn't open file {}: {}", full_path.display(), err))?;
+
+    let mut content = String::new();
+    file
+      .read_to_string(&mut content)
+      .map_err(|err| format!("Couldn't read file {}: {}", full_path.display(), err))?;
+
+    Ok(content)
+  }
+
+  fn list_objects(&self) -> Result<Vec<String>, String> {
+    let backup_file_regex = Regex::new(r"^backup_\w+_[0-9]+\.json$").map_err(|e| e.to_string())?;
+    let dir_content = read_dir(self.base_path.clone())
+      .map_err(|err| format!("Cannot read read datastore directory content: {}", err))?
+      .filter_map(|f| f.ok())
+      .filter(|f| f.file_type().is_ok() && f.file_type().unwrap().is_file())
+      .map(|f| f.file_name().to_string_lossy().to_string())
+      .filter(|f| backup_file_regex.is_match(f))
+      .collect();
+
+    Ok(dir_content)
+  }
+
+  fn put_object(path: &str, content: Vec<Bytes<u8>>) -> Result<(), String> {
+    todo!()
+  }
 }
