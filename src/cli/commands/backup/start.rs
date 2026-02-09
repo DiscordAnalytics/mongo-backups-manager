3e8b63869d9fc120aef933fc4d73e56c8520b5d9
Nolhan <nolhan.dev@gmail.com>
2026-02-09 15:40:17 +0100

    refactor(cli): enhanced variable declaration in `backup start` command

--- src/cli/commands/backup/start.rs
+++ src/cli/commands/backup/start.rs
@@ -7,16 +7,14 @@ use crate::{
 
 pub async fn start(name: String) {
   let config = Config::new();
-  let backup_config = config
+  let Some((_, backup_config)) = config
     .backups
     .iter()
-    .find(|(backup_name, _)| **backup_name == format!("backup.{name}"));
-
-  if backup_config.is_none() {
+    .find(|(backup_name, _)| **backup_name == format!("backup.{name}"))
+  else {
     Logger::error("Backup not found");
     exit(1);
-  }
-  let backup_config = backup_config.unwrap().1;
+  };
 
   Logger::info(format!("Starting backup job {}...", backup_config.display_name).as_str());
   match Daemon::start_backup_job(backup_config.clone()).await {
